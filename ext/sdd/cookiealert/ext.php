b463d303a74f79158e69936f05c12f7ad240ba1a
Grzegorz Świstak remote <grzegorz.swistak@orangeretail.pl>
2016-11-13 14:52:57 +0100

    Cookie alert - pierwsza modyfikacja

---
+++ ext/sdd/cookiealert/ext.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * Cookie Alert Pop-up. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016, SDD, http://podforak.pl
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace sdd\cookiealert;
+
+/**
+ * Cookie Alert Pop-up Extension base
+ *
+ * It is recommended to remove this file from
+ * an extension if it is not going to be used.
+ */
+class ext extends \phpbb\extension\base
+{
+}
